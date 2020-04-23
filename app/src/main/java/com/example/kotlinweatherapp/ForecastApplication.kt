250b729db4d1941de93ff038ec6c4109c2aee8c5
bogdan <bogdan@thesoftwarefarm.co.uk>
2020-04-24 01:05:59 +0300

    implemented repository & Kodein dependency injection

---
+++ app/src/main/java/com/example/kotlinweatherapp/ForecastApplication.kt
@@ -0,0 +1,26 @@
+package com.example.kotlinweatherapp
+
+import android.app.Application
+import com.example.kotlinweatherapp.data.db.ForecastDatabase
+import com.example.kotlinweatherapp.data.network.*
+import com.example.kotlinweatherapp.data.repository.ForecastRepository
+import com.example.kotlinweatherapp.data.repository.ForecastRepositoryImpl
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.androidXModule
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.instance
+import org.kodein.di.generic.singleton
+
+class ForecastApplication: Application(), KodeinAware {
+    override val kodein: Kodein = Kodein.lazy {
+        import(androidXModule(this@ForecastApplication))
+
+        bind() from singleton { ForecastDatabase(instance()) }
+        bind() from singleton { instance<ForecastDatabase>().currentWeatherDao() }
+        bind<ConnectivityInterceptor>() with singleton { ConnectivityInterceptorImpl(instance()) }
+        bind() from singleton { WeatherstackApiService(instance()) }
+        bind<WeatherNetworkDataSource>() with singleton { WeatherNetworkDataSourceImpl(instance()) }
+        bind<ForecastRepository>() with singleton { ForecastRepositoryImpl(instance(), instance()) }
+    }
+}
---
+++ app/src/main/java/com/example/kotlinweatherapp/data/repository/ForecastRepositoryImpl.kt
@@ -0,0 +1,54 @@
+package com.example.kotlinweatherapp.data.repository
+
+import android.icu.util.TimeZone
+import androidx.lifecycle.LiveData
+import com.example.kotlinweatherapp.data.db.CurrentWeatherDao
+import com.example.kotlinweatherapp.data.db.unitlocalized.UnitSpecificCurrentWeatherEntry
+import com.example.kotlinweatherapp.data.network.Response.CurrentWeatherResponse
+import com.example.kotlinweatherapp.data.network.WeatherNetworkDataSource
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.kodein.di.threadLocal
+import org.threeten.bp.ZonedDateTime
+import java.util.*
+
+class ForecastRepositoryImpl(
+    private val currentWeatherDao: CurrentWeatherDao,
+    private val weatherNetworkDataSource: WeatherNetworkDataSource
+) : ForecastRepository {
+
+    init {
+        weatherNetworkDataSource.downloadedCurrentWeather.observeForever { newCurrentWeather ->
+            persistFetchedCurrentWeather(newCurrentWeather)
+        }
+    }
+    override suspend fun getCurrentWeather(metric: Boolean): LiveData<out UnitSpecificCurrentWeatherEntry> {
+        return withContext(Dispatchers.IO) {
+            return@withContext if (metric) currentWeatherDao.getWeatherMetric()
+            else currentWeatherDao.getWeatherImperial()
+        }
+    }
+
+    private fun persistFetchedCurrentWeather(fetchedWeather: CurrentWeatherResponse) {
+        GlobalScope.launch(Dispatchers.IO) {
+            currentWeatherDao.upsert(fetchedWeather.currentWeatherEntry)
+        }
+    }
+
+    private suspend fun initWeatherData() {
+        if (isFetchCurrenNeeded(ZonedDateTime.now().minusHours(1)))
+            fetchCurrentWeather()
+    }
+
+    private suspend fun fetchCurrentWeather() {
+        weatherNetworkDataSource.fetchCurrentWeather("Los Angeles")
+    }
+
+    private fun isFetchCurrenNeeded(lastFetchTime: ZonedDateTime): Boolean {
+        val thirtyMinutesAgo = ZonedDateTime.now().minusMinutes(30)
+
+        return lastFetchTime.isBefore(thirtyMinutesAgo)
+    }
+}
